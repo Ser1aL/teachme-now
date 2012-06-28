b1fe5506fa925ff8ae8f51340d7275a354a95f43
Alexey Baranov <abaranov@productengine.com>
2012-06-28 19:24:01 +0300

    - add new_lesson and new_course forms

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -19,6 +19,27 @@ class LessonsController < ApplicationController
   def create
     # TODO
     # creates lesson/course
+    if params[:commit] == "Create Lesson"
+      lesson_params = params[:lesson]
+      date = "#{lesson_params['start_date(1i)']}-#{lesson_params['start_date(2i)']}-#{lesson_params['start_date(3i)']}"
+      time = "#{lesson_params['start_date(4i)']}:#{lesson_params['start_date(5i)']}"
+
+      Lesson.new(name: lesson_params[:name],
+                 city: lesson_params[:city],
+                 level: lesson_params[:level],
+                 duration: lesson_params[:hours].to_i + lesson_params[:minutes].to_i,
+                 description: lesson_params[:description],
+                 tease_description: lesson_params[:tease_description],
+                 capacity: lesson_params[:capacity],
+                 place_price: lesson_params[:place_price],
+                 start_date: date,
+                 start_time: time).save
+      render :action => "show"
+    else
+      @course = Course.new(params[:course])
+      @course.update_attribute(:owner_id, current_user.id)
+      render :action => "show"
+    end
   end
 
   def new
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ActiveRecord::Base
   attr_accessible :capacity, :city, :course_id, :description, :duration
   attr_accessible :interest_id, :level, :name, :owner_id, :place_price
-  attr_accessible :places_taken, :start_date, :start_time, :sub_interest_id, :tease_description
+  attr_accessible :places_taken, :start_date, :start_time, :sub_interest_id, :tease_description, :hours, :minutes
 
   belongs_to :interest
   belongs_to :sub_interest
@@ -18,6 +18,16 @@ class Lesson < ActiveRecord::Base
 
   validates_inclusion_of :level, in: %w(beginner low medium high expert)
 
-  SUPPORTED_CITIES = %w(odessa)
+  SUPPORTED_CITIES = %w(odessa kiev)
+  HOURS = [["1 hour", "60"], ["2 hours", "120"], ["3 hours", "180"], ["4 hours", "240"], ["5 hours", "300"],
+           ["6 hours", "360"], ["7 hours", "420"], ["8 hours", "480"], ["9 hours", "540"], ["10 hours", "600"],
+           ["11 hours", "660"], ["12 hours", "720"]]
+  MINUTES = [["15 minutes", "15"], ["30 minutes", "30"], ["45 minutes", "45"]]
+
+  def hours
+  end
+
+  def minutes
+  end
 
 end
