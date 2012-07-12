f3b14ab81c51c0964d11ad5120f17a5a7aa80c51
Alexey Baranov <abaranov@productengine.com>
2012-07-12 17:36:42 +0300

    - add methods in lesson_helper

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,14 @@
 module LessonsHelper
+
+  def select_level(lesson)
+    options_for_select(%w(beginner low medium high expert).map{ |option| [t("lesson_form.#{option}"), option] }, lesson.try(:level))
+  end
+
+  def select_hours(lesson)
+    options_for_select((0..12).map{|i| [pluralize(i, 'hour'), i] }, lesson.try(:duration).try(:/, 60))
+  end
+
+  def select_minutes(lesson)
+    options_for_select([0, 15, 30, 45].map{ |i| [ pluralize(i, 'minute'), i ] }, lesson.try(:duration).try(:%, 60))
+  end
 end
