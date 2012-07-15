4ec76f467f7842e85adfed401c1cfd23929377de
Max Reznichenko <max.reznichenko@gmail.com>
2012-07-15 19:02:01 +0300

    Forms refactoring

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,14 +1,14 @@
 module LessonsHelper
 
-  def select_level(lesson)
-    options_for_select(%w(beginner low medium high expert).map{ |option| [t("lesson_form.#{option}"), option] }, lesson.try(:level))
+  def skill_level_options(default_level = nil)
+    options_for_select(%w(beginner low medium high expert).map{ |option| [t("lesson_form.#{option}"), option] }, default_level)
   end
 
-  def select_hours(lesson)
-    options_for_select((0..12).map{|i| [pluralize(i, 'hour'), i] }, lesson.try(:duration).try(:/, 60))
+  def hours_options(default_duration = nil)
+    options_for_select((0..12).map{|i| [pluralize(i, 'hour'), i] }, default_duration.try(:/, 60))
   end
 
-  def select_minutes(lesson)
-    options_for_select([0, 15, 30, 45].map{ |i| [ pluralize(i, 'minute'), i ] }, lesson.try(:duration).try(:%, 60))
+  def minutes_options(default_duration = nil)
+    options_for_select([0, 15, 30, 45].map{ |i| [ pluralize(i, 'minute'), i ] }, default_duration.try(:%, 60))
   end
 end
