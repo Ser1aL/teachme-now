14a975a6d55b6bd793e7dd1068eb66192202a6b0
Max Reznichenko <max.reznichenko@gmail.com>
2012-10-06 15:55:17 +0300

    ff link border removed, record not found rescue, validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, with: ->{ redirect_to root_path, flash: { error: I18n.t('error.record_not_found') } }
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -19,11 +19,12 @@ class Lesson < ActiveRecord::Base
   validates_presence_of :city, :description, :tease_description
   validates_presence_of :start_datetime, :interest_id, :sub_interest_id
 
-  validates :capacity, presence: true, numericality: true
-  validates :place_price, presence: true, numericality: true
+  validates :capacity, presence: true
+  validates :place_price, presence: true
   validates :name, presence: true, uniqueness: true, length: { maximum: 140 }, format: { without: %r(^.*[\"\?\!\@\#\$\%\^\*\`\~\|/]+.*$) }
   validates :level, inclusion: { in: %w(beginner low medium high expert) }
   validates :duration, inclusion: { in: 15..765 }
+  validates_numericality_of :capacity, :place_price, greater_than: 0
 
   SUPPORTED_CITIES = %w(odessa)
   LESSONS_PER_PAGE = 3
