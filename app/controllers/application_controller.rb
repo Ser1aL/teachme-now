a3ae80c0b04287e93f521aa0d60e41a875e7200f
Max Reznichenko <max.reznichenko@gmail.com>
2012-10-06 19:52:55 +0300

    404, 500 redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from ActiveRecord::RecordNotFound, with: ->{ redirect_to root_path, flash: { error: I18n.t('error.record_not_found') } }
+  rescue_from ActiveRecord::RecordNotFound, with: ->{
+    # TODO change this to Static#not_found or Static#internal_error pages
+    redirect_to root_path, flash: { error: I18n.t('error.record_not_found') } unless request.xhr?
+  }
 end
