43a31b95536bd1aaf966a5d94fd3352eb052c636
Max Reznichenko <max@mreznichenko.(none)>
2012-10-18 19:17:30 +0300

    foreign user ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,14 +1,17 @@
 class RatingsController < ApplicationController
 
   before_filter :authenticate_user!
+  respond_to :json
 
   def create
-    Rating.find_or_create_by_giver_id_and_taker_id current_user.id, User.find(params[:user_id]).id
-    render text: 'Vote down'
+    taker = User.find(params[:user_id])
+    Rating.find_or_create_by_giver_id_and_taker_id current_user.id, taker.id
+    render json: { rating: taker.ratings.size }, layout: false
   end
 
   def update
-    Rating.find_by_giver_id_and_taker_id(current_user.id, User.find(params[:user_id]).id).destroy
-    render text: 'Vote up'
+    taker = User.find(params[:user_id])
+    Rating.find_by_giver_id_and_taker_id(current_user.id, taker.id).destroy
+    render json: { rating: taker.ratings.size }, layout: false
   end
 end
