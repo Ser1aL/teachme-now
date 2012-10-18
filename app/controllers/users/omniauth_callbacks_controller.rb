eccb38d5e8cfc14be2f73d03572a435f2fece385
Max Reznichenko <max@mreznichenko.(none)>
2012-10-18 17:57:42 +0300

    vk signin improvement

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,11 +9,11 @@ class Users::OmniauthCallbacksController < ApplicationController
   def vkontakte
     current_user = User.oauth_find_or_create(:vkontakte, request.env['omniauth.auth'], params[:code])
     sign_in current_user
-    render 'users/vk_close_window'
+    render 'users/vk_close_window', layout: false
   end
 
   def vkontakte_transitional
-    redirect_to root_path and return unless current_user
+    redirect_to new_user_session_path and return unless current_user
     if current_user.email.ends_with?('@vk.com')
       redirect_to user_update_email_path(current_user)
     else
@@ -24,6 +24,11 @@ class Users::OmniauthCallbacksController < ApplicationController
   def failure
     # TODO
     # create fallback page
-    redirect_to new_user_session_path
+    if params[:error]
+      render 'users/vk_close_window', layout: false
+    else
+      redirect_to new_user_session_path
+    end
+
   end
 end
