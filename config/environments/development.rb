3982fc1c0993bb440bb25e6d3322cae270255ee3
Max Reznichenko <max@mreznichenko.(none)>
2012-11-09 15:47:11 +0200

    devise locals and emails settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Teachme::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'teachme.com.ua' }
+  config.action_mailer.default_url_options = { :host => 'teach-me.com.ua' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
