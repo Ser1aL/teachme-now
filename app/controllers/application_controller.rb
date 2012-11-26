af99cd4ff6b917667807f3018027d347aaa62b14
Max Reznichenko <max@mreznichenko.(none)>
2012-11-26 20:13:15 +0200

    translation to RU

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, flash: { error: I18n.t('error.record_not_found') } unless request.xhr?
   }
 
+  before_filter :set_default_locale
   after_filter :update_current_user
 
 
@@ -13,6 +14,10 @@ class ApplicationController < ActionController::Base
 
   def update_current_user
     current_user.try :touch
+  end
+
+  def set_default_locale
+    I18n.locale = 'ru'
   end
 
 
