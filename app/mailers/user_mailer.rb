bc98ed295998e95d65ef7d1828ae2e8ee6913ac6
max <max.reznichenko@gmail.com>
2013-08-18 12:30:12 +0300

    mail to personal email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def feedback(user_info)
     @user_info = user_info
-    mail(to: 'support@teach-me.com.ua', subject: 'Feedback message')
+    mail(to: 'max.reznichenko@gmail.com', subject: 'Feedback message')
   end
 end
