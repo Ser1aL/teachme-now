a8f5b080ea9dba841b8972057dc500ee13ca7091
Alexey Baranov <abaranov@productengine.com>
2013-08-30 13:45:20 +0300

    - added concerns raw_sql_queries

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,8 @@ class User < ActiveRecord::Base
   validates_length_of :first_name, :last_name, minimum: 2, maximum: 22
   validates :phone, format: { with: /^[\(\)0-9\- \+\.]{7,20}$/ }, presence: true, unless: ->{ phone.blank? }
 
+  include RawSqlQueries
+
   VKONTAKTE_SEX_ASSOCIATIONS = {
     0 => 'unknown',
     1 => 'female',
@@ -169,50 +171,5 @@ class User < ActiveRecord::Base
       sizes[version_name.to_sym] = options[:uploader].processors[0][1].join('x')
     end
     image_attachment.try(:image).try(:url, size) || "http://placehold.it/#{sizes[size]}"
-  end
-
-  def self.find_teachers_info
-    create_teachers_info_table = '
-        CREATE OR REPLACE VIEW teachers_info_table AS
-        SELECT users.* , COUNT(users.id) AS total_lessons , SUM(lessons.duration)/60 AS total_hours , SUM(lessons.places_taken) AS total_students
-        FROM users
-        INNER JOIN shares
-        ON shares.user_id = users.id
-        INNER JOIN lessons
-        ON lessons.id = shares.lesson_id
-        WHERE shares.share_type = "teach"
-        GROUP BY users.id'
-
-    create_teachers_rating_table = '
-        CREATE OR REPLACE VIEW teachers_rating_table AS
-        SELECT ratings.taker_id, SUM(ratings.rating) AS teacher_rating
-        FROM ratings
-        WHERE ratings.taker_id IN
-        (
-          SELECT users.id
-          FROM users
-          INNER JOIN shares
-          ON shares.user_id = users.id
-          WHERE shares.share_type = "teach"
-          GROUP BY users.id
-        )
-        GROUP BY taker_id'
-
-    ActiveRecord::Base.connection.execute(create_teachers_info_table)
-    ActiveRecord::Base.connection.execute(create_teachers_rating_table)
-
-    self.find_by_sql(
-        %q{
-          SELECT *
-          FROM teachers_info_table
-          LEFT JOIN teachers_rating_table
-          ON teachers_info_table.id = teachers_rating_table.taker_id
-          UNION
-          SELECT *
-          FROM teachers_info_table
-          RIGHT JOIN teachers_rating_table
-          ON teachers_info_table.id = teachers_rating_table.taker_id
-        }
-    )
   end
 end
