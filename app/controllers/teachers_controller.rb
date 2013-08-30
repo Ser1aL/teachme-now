8bb074477cb4532cb73a0f018e66afc9b8ad1081
Max Reznichenko <mreznichenko@productengine.com>
2013-08-30 19:17:41 +0300

    teachers page refactoring

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,8 +1,8 @@
 class TeachersController < ApplicationController
 
-  def index
-    order, reverse = params[:order].try(:split, ' ') || :teacher_rating
+  before_filter :set_order, :verify_acceptable_order
 
+  def index
     teachers = User.find_teachers_info
     teachers.each{|teacher| teacher.teacher_rating = 0 if teacher.teacher_rating == nil}
 
@@ -14,13 +14,31 @@ class TeachersController < ApplicationController
 
     @teachers_count = teachers.size
 
-    teachers.sort_by!(&order.to_sym).reverse!
-    teachers.reverse! if reverse
+    teachers.sort_by!(&@order.to_sym).reverse!
+    teachers.reverse! if @reverse
     @teachers = Kaminari.paginate_array(teachers).page(params[:page]).per(8)
   end
 
   def search
     @lessons = Lesson.slow_search(params[:query]).by_page(params[:page])
     render :index
+  end
+
+  private
+
+  def set_order
+    @order, @reverse = if params[:order] =~ /(.*)_reverse/
+      [$1, true]
+    elsif params[:order].blank?
+      [:teacher_rating, false]
+    else
+      [params[:order], false]
+    end
+  end
+
+  def verify_acceptable_order
+    unless %w(teacher_rating total_lessons total_hours total_students).include?(@order.to_s)
+      @order, @reverse = :teacher_rating, false
+    end
   end
 end
