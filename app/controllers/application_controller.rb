ca7e6be1533d992f06d4115ef1ad6c52b4242487
Max Reznichenko <mreznichenko@productengine.com>
2013-09-04 16:50:17 +0300

    removing greet page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   prepend_before_filter :update_current_user
-  before_filter :set_dev, :redirect_if_not_released
   before_filter :preload_interest_tree
 
   private
@@ -13,16 +12,6 @@ class ApplicationController < ActionController::Base
 
   def preload_interest_tree
     @interests = Interest.includes(:sub_interests)
-  end
-
-  def redirect_if_not_released
-    unless Rails.env.development?
-      redirect_to '/greet' if session['dev'].blank? && params[:action] != 'greet'
-    end
-  end
-
-  def set_dev
-    session['dev'] = 'dev login' if params[:let_me_in]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -77,6 +77,5 @@ Teachme::Application.routes.draw do
   match '/404', to: 'errors#not_found'
   match '/500', to: 'errors#internal_server_error'
 
-  get '/greet', to: 'home#greet'
   root to: 'home#index'
 end
