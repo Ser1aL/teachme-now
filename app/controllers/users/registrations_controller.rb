b3e83a286443ca2aa98b4ed470a4ce3cd6282921
Max Reznichenko <mreznichenko@productengine.com>
2013-10-03 20:21:43 +0300

    added user mailer welcome message

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,6 +11,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
+        UserMailer.welcome(resource).deliver
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
