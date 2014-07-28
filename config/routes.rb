3eb59286b12de7838a99145062cc0f4ee5ccbc98
Alexey Baranov <abaranov@productengine.com>
2014-07-28 19:27:46 +0300

    Implemented the Interests page for the admin(create, update, delete).

--- config/routes.rb
+++ config/routes.rb
@@ -84,6 +84,7 @@ Teachme::Application.routes.draw do
 
     resources :courses, only: %w(index)
     resources :email_distributions, only: %w(new create)
+    resources :interests, except: %w(show)
   end
 
   devise_for :users, path_prefix: 'd', controllers: {
